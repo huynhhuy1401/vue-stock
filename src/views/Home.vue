8b7070a4edf1010c11f76bc2d0196cfd60dd9697
Huy Duong <duonghuy1401@gmail.com>
2020-03-09 20:56:18 +0700

    Add more instruction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>Stock trading website</h1>
-    <p>Instruction: Click <span class="bold">Stock</span> to buy stocks and click <span class="bold">Portfolio</span> to check your stocks and sell it.</p>
+    <p>
+      Instruction: Click
+      <span class="bold">Stock</span> to buy stocks and click
+      <span class="bold">Portfolio</span> to check your stocks and sell it. Click
+      <span class="bold">End day</span> to random the stocks price
+    </p>
   </div>
 </template>
 
