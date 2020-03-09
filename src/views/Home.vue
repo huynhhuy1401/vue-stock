cc748d461b6d7f26c4af1b76aacf330c76b9ed7e
Huy Duong <duonghuy1401@gmail.com>
2020-03-09 20:50:07 +0700

    Fix grammar mistakes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Stock trading website</h1>
-    <p>Instruction: Click <span class="bold">Stock</span> to buy stocks and click <span class="bold">Portfolio</span> to check your stock and sell it.</p>
+    <p>Instruction: Click <span class="bold">Stock</span> to buy stocks and click <span class="bold">Portfolio</span> to check your stocks and sell it.</p>
   </div>
 </template>
 
